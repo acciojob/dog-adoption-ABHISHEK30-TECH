c254c50098fc4eca2c54e2b4aa8baed7461328d6
ABHISHEK30-TECH <abhishekumar4165@gmail.com>
2025-01-07 23:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your code here
+<a href="https://en.wikipedia.org/wiki/Golden_Retriever">Retriever</a>
+<li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Full_attention_%288067543690%29.jpg/1280px-Full_attention_%288067543690%29.jpg"></li>
